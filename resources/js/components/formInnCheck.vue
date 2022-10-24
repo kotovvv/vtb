edeb6369ab18f5f3c7b78e524a7339a8bf9670da
kotovvv <kotovvv@gmail.com>
2022-10-24 14:09:25 +0300

    first commit

---
+++ resources/js/components/formInnCheck.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">Проверка ИНН в ВТБ</div>
+          <div class="card-body">
+              <label for="inn" class="hidden-visually">ИНН: </label>
+              <input type="number" name="inn" id="inn" v-model="inn" />
+              <button type="submit" @click="checkInn">Проверить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'InnCheck',
+  mounted() {
+    console.log("InnCheck component mounted");
+  },
+  data:()=>({
+    inn:'',
+  }),
+  methods: {
+    checkInn(){
+        console.log(this.inn)
+    }
+  },
+};
+</script>
